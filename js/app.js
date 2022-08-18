e9d192e9217262cf78e6aaeafe663f0cea32cdd0
Manuch Sadri <colonquebang@hotmail.com>
2022-08-17 18:33:18 -0700

    shown and vote incrementers now functioning. Though images are being displayed one too many times.

--- js/app.js
+++ js/app.js
@@ -6,8 +6,9 @@
 let voteCount = 0; // user's product vote counter
 let maxVotes = 5; // maximum number of product voting rounds
 let displayQty = 3; // number of products to be displayed at one time for voting
-let newProducts = [];
-let allProducts = [ // array of new Product objects where object name equals name of img asset
+let prodObjs = []; // arrary of product objects
+let allProducts = [ // eslint-disable-line
+    // array of Product constructor calls where argumment value equals name of img asset
     new Product('bag.jpg'), new Product('banana.jpg'), new Product('bathroom.jpg'), new Product('boots.jpg'),
     new Product('breakfast.jpg'), new Product('bubblegum.jpg'), new Product('chair.jpg'), new Product('cthulhu.jpg'),
     new Product('dog-duck.jpg'), new Product('dragon.jpg'), new Product('pen.jpg'), new Product('pet-sweep.jpg'),
@@ -15,15 +16,6 @@ let allProducts = [ // array of new Product objects where object name equals nam
     new Product('unicorn.jpg'), new Product('water-can.jpg'), new Product('wine-glass.jpg'),
 ];
 
-let shown  = [];
-for (let i = 0; i < allProducts.length; i++) {
-    shown.push(allProducts[i].countShown);
-}
-
-let votes  = [];
-for (let i = 0; i < allProducts.length; i++) {
-    votes.push(allProducts[i].countVoted);
-}
 
 // Product constructor
 function Product(fileName) { // accepts new product as a filename e.g. bag.jpg
@@ -31,30 +23,21 @@ function Product(fileName) { // accepts new product as a filename e.g. bag.jpg
     this.filePath = `img/${fileName}`; // storage location of product image file
     this.countShown = 0; // counts number of times the product has been displayed for voting
     this.countVoted = 0; // counts number of times the product has been chosen by the user
-    newProducts.push(this);
+    prodObjs.push(this);
 }
 
 // function to return a random index for allProducts array
 function rPG() { // the notorious Random Product Generator (this isn't nearly as clever as I think it is)
-    // returns a whole number 0 - index of last item in allProducts array
-    return Math.floor(Math.random() * allProducts.length);
-}
-
-// Event listener for product voting buttons
-let buttons = [document.getElementById('voteButton0'), document.getElementById('voteButton1'), document.getElementById('voteButton2')];
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', loadImages);}
-
-// Event handler to show new image
-function loadImages(event) {
 
     let products = []; // array to hold selected products to be displayed to user for voting
 
     // generate disaplyQty number of randomly selected unique product images to be displayed to user
     while (products.length < displayQty) {
-        let index = rPG();
-        while (!products.includes(allProducts[index])) {
-            products.push(allProducts[index]);
+        // returns a whole number 0 - index of last item in allProducts array
+        let index = Math.floor(Math.random() * prodObjs.length);
+        while (!products.includes(prodObjs[index])) {
+            products.push(prodObjs[index]);
+            prodObjs[index].countShown++;
         }
     }
 
@@ -64,29 +47,55 @@ function loadImages(event) {
         img.src = products[i].filePath;
         img.alt = products[i].name;
         img.title = products[i].name;
-        newProducts[i].countShown++;
     }
+}
+
+// Event listener for product voting buttons
+let buttons = [document.getElementById('voteButton0'), document.getElementById('voteButton1'), document.getElementById('voteButton2')];
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', voteCounter);
+}
+
+// Event handler to increment vote trackers & remove event listener after maxVotes is reached
+function voteCounter(event) {
 
     // logic to increment vote counters
     if (event){
-        let target = event.target.alt;
-        //newProducts[newProducts.indexOf(target)].countVoted++;
         voteCount++;
-
+        let target = event.target.alt;
         console.log('target ' + target);
-        //console.log(event.target);
 
-        // logic for when voting rounds have been completed:
+        for (let i = 0; i < prodObjs.length; i++) {
+            if (target === prodObjs[i].name) {
+                prodObjs[i].countVoted++;
+                //console.log('prodObjs[i] = ' +  )
+                break;
+            }
+        }
+
+        // logic for when voting rounds have been completed to remove the event handler
         if (voteCount === maxVotes) {
             for (let i = 0; i < buttons.length; i++) {
-                buttons[i].removeEventListener('click', loadImages);
+                buttons[i].removeEventListener('click', voteCounter);
             }
         }
     }
 
-    console.log(voteCount);
+    rPG();
+
+    console.log('voteCount ' + voteCount);
+
+    let shown  = [];
+    for (let i = 0; i < prodObjs.length; i++) {
+        shown.push(prodObjs[i].countShown);
+    }
     console.log('shown ' + shown);
+
+    let votes  = [];
+    for (let i = 0; i < prodObjs.length; i++) {
+        votes.push(prodObjs[i].countVoted);
+    }
     console.log('votes ' + votes);
 }
 
-loadImages();
+voteCounter();
